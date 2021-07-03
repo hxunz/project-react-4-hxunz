4b2a0b51d095025d080ac8eb5fbe57c4a8bc823b
hxunz <guswl2843@gmail.com>
2021-07-03 13:22:34 +0900

    ➕ Create DailyRoutine component

---
+++ src/DailyRoutine.jsx
@@ -0,0 +1,11 @@
+aboutimport React from 'react';
+
+export default function DailyRoutine() {
+  return (
+    <p>
+      <button type="button" onClick={onclick}>
+        Daily Routine
+      </button>
+    </p>
+  );
+}
