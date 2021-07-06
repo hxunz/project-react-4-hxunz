7e01de95bb39c87dc8b9960bbe0a7221299c1f53
hxunz <guswl2843@gmail.com>
2021-07-06 22:06:55 +0900

    ➕ Create App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-export default function App() {
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
 
+import DailyRoutine from './DailyRoutine';
+import DailyRoutineList from './DailyRoutineList';
+import NotFoundPage from './NotFoundPage';
+
+export default function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={DailyRoutine} />
+      <Route path="/DailyRoutineList" component={DailyRoutineList} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  );
 }
