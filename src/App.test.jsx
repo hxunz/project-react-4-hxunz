917d4c60d8825a4bd0088d085093f624905a69d0
hxunz <guswl2843@gmail.com>
2021-07-06 22:04:28 +0900

    🧪 Add test code about App

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import {
+  MemoryRouter,
+} from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('App', () => {
+describe('App', () => {
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
 
+  context('with path/', () => {
+    it('renders the DailyRoutine', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('Daily Routine');
+    });
+  });
+
+  context('with invalid path', () => {
+    it('renders the not found page', () => {
+      const { container } = renderApp({ path: '/xxx' });
+
+      expect(container).toHaveTextContent('Not Found');
+    });
+  });
 });
