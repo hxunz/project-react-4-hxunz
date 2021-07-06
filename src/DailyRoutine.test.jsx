de27d118efb74e5f3961badfbc688454a5ca28a8
hxunz <guswl2843@gmail.com>
2021-07-06 22:08:06 +0900

    ♻️ Refactor test code

--- src/DailyRoutine.test.jsx
+++ src/DailyRoutine.test.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import {
+  MemoryRouter,
+} from 'react-router-dom';
+
+import { render } from '@testing-library/react';
 
 import DailyRoutine from './DailyRoutine';
 
-test('DailyRoutine', () => {
-  const { getByText } = render((
-    <DailyRoutine />
-  ));
+describe('DailyRoutine', () => {
+  function renderDailyRoutine({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <DailyRoutine />
+      </MemoryRouter>
+    ));
+  }
 
-  fireEvent.click(getByText(/Daily Routine/));
+  context('with path /DailyRoutineList', () => {
+    it('renders the DailyRoutineList page', () => {
+      const { container } = renderDailyRoutine({ path: '/DailyRoutineList' });
+
+      expect(container).toHaveTextContent('Daily Routine');
+    });
+  });
 });
