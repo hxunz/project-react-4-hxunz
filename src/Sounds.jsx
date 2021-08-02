3eb7c53b4c82be20374cc4267b60366b093cf309
hxunz <guswl2843@gmail.com>
2021-08-02 12:22:38 +0900

    ✨ Design play buttons

--- src/Sounds.jsx
+++ src/Sounds.jsx
@@ -2,11 +2,86 @@ import React from 'react';
 
 import Fab from '@material-ui/core/Button';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import ReactAudioPlayer from 'react-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 export default function Sounds() {
+  const styles = {
+    rain: {
+      height: 135,
+      width: 500,
+      margin: 20,
+      justifyContent: 'center',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundImage: `url(${'image/rainy.jpg'})`,
+    },
+    fire: {
+      height: 135,
+      width: 500,
+      margin: 20,
+      justifyContent: 'center',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundImage: `url(${'image/fire.jpg'})`,
+    },
+    ocean: {
+      height: 135,
+      width: 500,
+      margin: 20,
+      justifyContent: 'center',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundImage: `url(${'image/ocean.jpg'})`,
+    },
+    forest: {
+      height: 135,
+      width: 500,
+      margin: 20,
+      justifyContent: 'center',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundImage: `url(${'image/forest.jpg'})`,
+    },
+  };
+
   return (
     <>
-      <Fab role="button" color="primary" aria-label="play">
+      <Fab
+        role="button"
+        color="white"
+        aria-label="play"
+        style={styles.rain}
+      >
+        <ReactAudioPlayer
+          src="sound/rain.mp3"
+          autoPlay
+          controls
+        />
+        <PlayCircleOutlineIcon />
+      </Fab>
+      <Fab
+        role="button"
+        color="white"
+        aria-label="play"
+        style={styles.fire}
+      >
+        <PlayCircleOutlineIcon />
+      </Fab>
+      <Fab
+        role="button"
+        color="white"
+        aria-label="play"
+        style={styles.ocean}
+      >
+        <PlayCircleOutlineIcon />
+      </Fab>
+      <Fab
+        role="button"
+        color="white"
+        aria-label="play"
+        style={styles.forest}
+      >
         <PlayCircleOutlineIcon />
       </Fab>
     </>
